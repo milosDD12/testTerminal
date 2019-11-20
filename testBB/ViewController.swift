5b295595a57541542191961639e70df327b390b6
Milos <milos@darwindigital.com>
2019-11-20 15:14:53 +0100

    Initial Commit

---
+++ testBB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testBB
+//
+//  Created by Milos Stevanovic on 20/11/2019.
+//  Copyright © 2019 Milos Stevanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
